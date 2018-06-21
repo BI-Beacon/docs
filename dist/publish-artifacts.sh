c2157f86dda947ee4d3c451bb06d90a2c8ba57d7
Martin Kjellstrand <github@madworx.se>
2018-06-21 18:15:36 +0200

    Exclude build-related files in publishing -> artifacts. (#8)
    * Exclude build-related files in publishing -> artifacts.
    * Debug output.
    * Add generated architecture.png -> .gitignore.

--- dist/publish-artifacts.sh
+++ dist/publish-artifacts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ "${TRAVIS_BRANCH}" == "master" ] ; then
+    echo "On \`master' branch - pushing to artifacts repository."
     TMPDR="$(mktemp -d)"
     
     git config --global user.email "travis@travis-ci.org"
@@ -10,9 +11,14 @@ if [ "${TRAVIS_BRANCH}" == "master" ] ; then
 
     (cd "${TMPDR}" && git rm -r docs/* ; mkdir docs )
 
-    cp -a docs/* "${TMPDR}/docs/"
+    find . -name '*.rst' -o -path './docs/_build/*' -o -path './docs/_static/*' -print0 \
+      | tar --null -T /dev/stdin | ( "cd ${TMPDIR}/docs" && tar xvf - )
+
     cd "${TMPDR}" || exit 1
     git add -A 
     git commit -a -m 'Automated build'
     git push
+else
+    echo "Not on \`master' branch - not publishing."
 fi
+
